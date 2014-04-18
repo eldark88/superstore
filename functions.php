0dde98461b8d2d4536a3fba0bf4714081ee2fc88
Eldar Khalyknazarov <eldar@wellmatchhealth.com>
2014-04-17 20:37:45 -0500

    translation

--- functions.php
+++ functions.php
@@ -35,9 +35,19 @@ foreach ( $includes as $i ) {
 /*-----------------------------------------------------------------------------------*/
 
 
+add_filter( 'add_to_cart_text', 'woo_custom_cart_button_text' );                                
+add_filter( 'woocommerce_product_single_add_to_cart_text', 'woo_custom_cart_button_text' );    
+ 
+function woo_custom_cart_button_text() {
+    return 'Добавить';
+}
 
-
-
+add_filter( 'add_to_cart_text', 'woo_custom_cart_button_text' );                       
+add_filter( 'woocommerce_product_add_to_cart_text', 'woo_custom_cart_button_text' );    
+ 
+function woo_custom_cart_button_text() {
+    return 'Добавить';
+}
 
 
 
